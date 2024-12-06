78f56586fb245592146e0e74cbe2481e8d8d9a98
KP45 <kevinpan45@163.com>
2024-12-06 17:31:46 +0800

    flyway work

--- src/main/java/tech/kp45/bids/bridge/pipeline/workflow/Workflow.java
+++ src/main/java/tech/kp45/bids/bridge/pipeline/workflow/Workflow.java
@@ -1,9 +1,28 @@
 package tech.kp45.bids.bridge.pipeline.workflow;
 
+import java.util.Date;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
+@TableName("workflow")
 public class Workflow {
+    @TableId
     private Integer id;
+    @TableField("`dag`")
     private String dag;
+    @TableField("created_at")
+    private Date createdAt;
+    @TableField("updated_at")
+    private Date updatedAt;
+    @TableField("deleted")
+    private boolean deleted;
+    @TableField("deleted_at")
+    private Date deletedAt;
 }
