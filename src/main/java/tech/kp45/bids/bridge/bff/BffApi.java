8719891ee51ccbd57bcbdd05cc0855d182078eb0
KP45 <kevinpan45@163.com>
2024-12-19 17:39:12 +0800

    storage bff api implement and test script

--- src/main/java/tech/kp45/bids/bridge/bff/BffApi.java
+++ src/main/java/tech/kp45/bids/bridge/bff/BffApi.java
@@ -1,13 +1,12 @@
 package tech.kp45.bids.bridge.bff;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +18,6 @@ import tech.kp45.bids.bridge.dataset.storage.provider.MinioBidsStorageDal;
 @Slf4j
 @RestController
 public class BffApi {
-
-    @Autowired
-    private RedisTemplate<String, String> redisTemplate;
-
-    private static final String BIDS_STORAGE_KEY = "bids:storage:";
 
     @Autowired
     private BidsStorageRegister bidsStorageRegister;
@@ -38,22 +32,6 @@ public class BffApi {
         return find(id);
     }
 
-    @PutMapping("/api/bids/storages/{id}/datasets")
-    public void cacheStorageBids(@PathVariable Integer id) {
-        BidsStorage storage = find(id);
-        if (storage == null) {
-            log.error("Storage {} not found", id);
-            throw new BasicRuntimeException("Storage not found");
-        }
-
-        MinioBidsStorageDal dal = new MinioBidsStorageDal(storage);
-        List<String> datasets = dal.listBidsPath();
-        datasets.forEach(dataset -> {
-            String key = BIDS_STORAGE_KEY + id + ":" + dataset.replace("/", "");
-            redisTemplate.opsForValue().set(key, dataset);
-        });
-    }
-
     @GetMapping("/api/bids/storages/{id}/datasets")
     public String listStorageBids(@PathVariable Integer id) {
         BidsStorage storage = find(id);
@@ -62,19 +40,28 @@ public class BffApi {
             throw new BasicRuntimeException("Storage not found");
         }
 
-        final List<String> datasets = new ArrayList<>();
-        if (redisTemplate.opsForValue().getOperations().hasKey(BIDS_STORAGE_KEY + id + ":*")) {
-            redisTemplate.opsForValue().getOperations().keys(BIDS_STORAGE_KEY + id + ":*").forEach(key -> {
-                datasets.add(redisTemplate.opsForValue().get(key));
-            });
-        } else {
-            MinioBidsStorageDal dal = new MinioBidsStorageDal(storage);
-            dal.listBidsPath().stream().forEach(dataset -> {
-                datasets.add(dataset.replace("/", ""));
-            });
-        }
+        List<String> datasets = new ArrayList<>();
+
+        MinioBidsStorageDal dal = new MinioBidsStorageDal(storage);
+        dal.listBidsPath().stream().forEach(dataset -> {
+            datasets.add(dataset.replace("/", ""));
+        });
 
         return datasets.toString();
+    }
+
+    @GetMapping("/api/bids/storages/{id}/datasets/{dataset}/files")
+    public List<String> listDatasetFiles(@PathVariable Integer id, @PathVariable String dataset) {
+        BidsStorage storage = find(id);
+        if (storage == null) {
+            log.error("Storage {} not found", id);
+            throw new BasicRuntimeException("Storage not found");
+        }
+
+        MinioBidsStorageDal dal = new MinioBidsStorageDal(storage);
+        List<String> files = new ArrayList<>();
+        dal.scanFiles(dataset + File.separator, files);
+        return files;
     }
 
     private BidsStorage find(Integer id) {
