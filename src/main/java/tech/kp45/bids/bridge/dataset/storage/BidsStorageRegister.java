ac53ee04cf27b20cd568bfa8fd203976fd69e774
KP45 <kevinpan45@163.com>
2024-12-20 10:44:48 +0800

    desensitization bids storage

--- src/main/java/tech/kp45/bids/bridge/dataset/storage/BidsStorageRegister.java
+++ src/main/java/tech/kp45/bids/bridge/dataset/storage/BidsStorageRegister.java
@@ -1,17 +1,30 @@
 package tech.kp45.bids.bridge.dataset.storage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import cn.hutool.core.bean.BeanUtil;
 import lombok.Getter;
 import lombok.Setter;
 
 @Component
 @ConfigurationProperties(prefix = "bids")
 public class BidsStorageRegister {
-    @Getter
     @Setter
     private List<BidsStorage> storages;
+
+    public List<BidsStorage> getStorages() {
+        List<BidsStorage> desensitization = new ArrayList<>();
+        storages.forEach(storage -> {
+            BidsStorage clone = new BidsStorage();
+            BeanUtil.copyProperties(storage, clone, false);
+            clone.setAccessKey(null);
+            clone.setSecretKey(null);
+            desensitization.add(clone);
+        });
+        return desensitization;
+    }
 }
