31e138ff7fd7945956bf14531dcaec046db1f27f
KP45 <kevinpan45@163.com>
2024-12-20 11:11:35 +0800

    fix API bug

--- src/main/java/tech/kp45/bids/bridge/bff/BffApi.java
+++ src/main/java/tech/kp45/bids/bridge/bff/BffApi.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import tech.kp45.bids.bridge.dataset.storage.provider.MinioBidsStorageDal;
 
 @Slf4j
 @RestController
+@CrossOrigin
 public class BffApi {
 
     @Autowired
@@ -33,7 +35,7 @@ public class BffApi {
     }
 
     @GetMapping("/api/bids/storages/{id}/datasets")
-    public String listStorageBids(@PathVariable Integer id) {
+    public List<String> listStorageBids(@PathVariable Integer id) {
         BidsStorage storage = find(id);
         if (storage == null) {
             log.error("Storage {} not found", id);
@@ -47,7 +49,7 @@ public class BffApi {
             datasets.add(dataset.replace("/", ""));
         });
 
-        return datasets.toString();
+        return datasets;
     }
 
     @GetMapping("/api/bids/storages/{id}/datasets/{dataset}/files")
