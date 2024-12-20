1052823953b7bf8b6355a450e89b61f5ff521315
KP45 <kevinpan45@163.com>
2024-12-20 19:20:24 +0800

    fix log entity

--- src/main/java/tech/kp45/bids/bridge/common/web/GlobalApiExcetionHandler.java
+++ src/main/java/tech/kp45/bids/bridge/common/web/GlobalApiExcetionHandler.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
 import tech.kp45.bids.bridge.common.exception.BasicException;
 import tech.kp45.bids.bridge.common.exception.BasicRuntimeException;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalApiExcetionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(MultipartException.class)
@@ -19,18 +21,18 @@ public class GlobalApiExcetionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BasicRuntimeException.class)
     public ResponseEntity<String> handleBusinessRuntimeError(BasicRuntimeException e) {
-        logger.error("Server Internal Runtime Error", e);
+        log.error("Server Internal Runtime Error", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("System Internal error.");
     }
 
     @ExceptionHandler(BasicException.class)
     public ResponseEntity<String> handleBusinessServerError(BasicException e) {
-        logger.error("Server Internal Error", e);
+        log.error("Server Internal Error", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("System Internal error.");
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleServerError() {
+    public ResponseEntity<String> handleServerError(Exception e) {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE.value()).body("System Internal error.");
     }
 }
