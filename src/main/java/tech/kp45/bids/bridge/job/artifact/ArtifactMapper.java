16c2cac84c66a3d82ac1ce54e69b4eced5b4ca67
KP45 <kevinpan45@163.com>
2025-02-08 11:52:08 +0800

    add job artifact service

---
+++ src/main/java/tech/kp45/bids/bridge/job/artifact/ArtifactMapper.java
@@ -0,0 +1,7 @@
+package tech.kp45.bids.bridge.job.artifact;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface ArtifactMapper extends BaseMapper<Artifact> {
+
+}
---
+++ src/main/java/tech/kp45/bids/bridge/job/artifact/ArtifactService.java
@@ -0,0 +1,22 @@
+package tech.kp45.bids.bridge.job.artifact;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+
+@Service
+public class ArtifactService {
+    @Autowired
+    private ArtifactMapper artifactMapper;
+
+    public Artifact findByJob(Integer jobId) {
+        LambdaQueryWrapper<Artifact> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Artifact::getJobId, jobId);
+        return artifactMapper.selectOne(queryWrapper);
+    }
+
+    public void create(Artifact artifact) {
+        artifactMapper.insert(artifact);
+    }
+}
