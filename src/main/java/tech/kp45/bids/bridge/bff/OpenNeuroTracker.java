b7a9f3b11dd2a70060f99bba4828970d2ec89a80
KP45 <kevinpan45@163.com>
2025-02-20 11:19:49 +0800

    use redis stream as mq and remove kafka dependencies

--- src/main/java/tech/kp45/bids/bridge/bff/OpenNeuroTracker.java
+++ src/main/java/tech/kp45/bids/bridge/bff/OpenNeuroTracker.java
@@ -7,13 +7,13 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.connection.stream.StreamOffset;
+import org.springframework.data.redis.connection.stream.StreamRecords;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.util.StringUtils;
 
-import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
 import tech.kp45.bids.bridge.dataset.accessor.BidsDataset;
 import tech.kp45.bids.bridge.dataset.accessor.provider.OpenNeuroAccessor;
@@ -29,8 +29,6 @@ public class OpenNeuroTracker {
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
     @Value("${bids.collector.openneuro.sync.enabled}")
     private boolean enabled;
 
@@ -46,7 +44,10 @@ public class OpenNeuroTracker {
             if (acquired) {
                 List<BidsDataset> bidses = accessor.scan();
                 for (BidsDataset bids : bidses) {
-                    kafkaTemplate.send(OpenNeuroAccessor.OPENNEURO_BIDS_TRACK_TOPIC, JSONUtil.toJsonStr(bids));
+                    ObjectRecord<String, BidsDataset> record = StreamRecords.newRecord()
+                            .in(OpenNeuroAccessor.OPENNEURO_BIDS_TRACK_TOPIC)
+                            .ofObject(bids);
+                    redisTemplate.opsForStream().add(record);
                 }
             }
         } finally {
@@ -56,23 +57,28 @@ public class OpenNeuroTracker {
         }
     }
 
-    @KafkaListener(topics = OpenNeuroAccessor.OPENNEURO_BIDS_TRACK_TOPIC, groupId = "bids-openneuro-tracker")
-    public void listenOnOpenNeuroTrackEvent(String message) {
-        BidsDataset bids = JSONUtil.toBean(message, BidsDataset.class);
-        List<String> files = new ArrayList<>();
-        try {
-            accessor.scanFiles(bids.getStoragePath(), files);
-        } catch (Exception e) {
-            log.error("Failed to scan files for BIDS dataset {} : {}", bids.getStoragePath(), e.getMessage());
-            return;
-        }
-        String bidsFilesKey = "bids:dataset:openneuro:tracking:" + bids.getDoi() + ":" + bids.getVersion() + ":files:";
-        files.stream().forEach(file -> {
-            String filename = StringUtils.getFilename(file);
-            String fileKey = bidsFilesKey + filename;
-            if (!redisTemplate.hasKey(fileKey)) {
-                redisTemplate.opsForValue().set(fileKey, file);
+    @Scheduled(cron = "${bids.collector.openneuro.track.cron}")
+    public void listenOnOpenNeuroTrackEvent() {
+        List<ObjectRecord<String, BidsDataset>> records = redisTemplate.opsForStream()
+                .read(BidsDataset.class, StreamOffset.fromStart(OpenNeuroAccessor.OPENNEURO_BIDS_TRACK_TOPIC));
+        records.stream().forEach(record -> {
+            BidsDataset bids = record.getValue();
+            List<String> files = new ArrayList<>();
+            try {
+                accessor.scanFiles(bids.getStoragePath(), files);
+            } catch (Exception e) {
+                log.error("Failed to scan files for BIDS dataset {} : {}", bids.getStoragePath(), e.getMessage());
+                return;
             }
+            String bidsFilesKey = "bids:dataset:openneuro:tracking:" + bids.getDoi() + ":" + bids.getVersion()
+                    + ":files:";
+            files.stream().forEach(file -> {
+                String filename = StringUtils.getFilename(file);
+                String fileKey = bidsFilesKey + filename;
+                if (!redisTemplate.hasKey(fileKey)) {
+                    redisTemplate.opsForValue().set(fileKey, file);
+                }
+            });
         });
     }
 }
