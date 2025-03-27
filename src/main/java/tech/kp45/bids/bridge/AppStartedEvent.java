c3e663f277b3826d5ead1e055509772484f45e3d
KP45 <kevinpan45@163.com>
2025-03-27 23:47:21 +0800

    cancel auto import bids apps to database

--- src/main/java/tech/kp45/bids/bridge/AppStartedEvent.java
+++ src/main/java/tech/kp45/bids/bridge/AppStartedEvent.java
@@ -1,6 +1,5 @@
 package tech.kp45.bids.bridge;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -10,7 +9,6 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
-import tech.kp45.bids.bridge.bff.BidsAppsAccessor;
 import tech.kp45.bids.bridge.dataset.accessor.BidsDataset;
 import tech.kp45.bids.bridge.dataset.accessor.provider.OpenNeuroAccessor;
 
@@ -19,15 +17,11 @@ import tech.kp45.bids.bridge.dataset.accessor.provider.OpenNeuroAccessor;
 public class AppStartedEvent implements ApplicationListener<ApplicationReadyEvent> {
     @Autowired
     private OpenNeuroAccessor openNeuroAccessor;
-    @Autowired
-    private BidsAppsAccessor bidsAppsAccessor;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         // Cache OpenNeuro
         openneuroBidsInit();
-        // import and save BIDS Apps to database
-        bidsAppsAccessor.importBidsApps();
     }
 
     private void openneuroBidsInit() {
