369733b328c0774db6181cf2c71696a994d3efec
KP45 <kevinpan45@163.com>
2025-05-04 15:41:13 +0800

    DataProvider add CCNDC

--- src/main/java/tech/kp45/bids/bridge/dataset/DatasetProvider.java
+++ src/main/java/tech/kp45/bids/bridge/dataset/DatasetProvider.java
@@ -1,7 +1,8 @@
 package tech.kp45.bids.bridge.dataset;
 
 public enum DatasetProvider {
-    OPENNEURO("OpenNeuro", "OpenNeuro is a platform for sharing and analyzing neuroimaging data."),;
+    OPENNEURO("OpenNeuro", "OpenNeuro is a platform for sharing and analyzing neuroimaging data."),
+    CCNDC("CCNDC", "中国人彩巢数据社区，促进全生命周期脑智健康发展");
 
     private final String name;
     private final String description;
